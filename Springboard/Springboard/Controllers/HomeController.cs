61b06e2096104d53307285c22174681f835695db
Jon Pitzen <jtpitzen@gmail.com>
2016-02-11 18:47:15 -0600

    Removed default Microsoft stuff for demo

--- Springboard/Springboard/Controllers/HomeController.cs
+++ Springboard/Springboard/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Springboard.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Springboard About Page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Info.";
 
             return View();
         }
